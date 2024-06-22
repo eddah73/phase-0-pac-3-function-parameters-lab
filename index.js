8ea79f16891b0f9a2c6e7f08eff7742179a64095
eddah chepkoech <eddahsjr@gmail.com>
2024-06-22 20:30:19 +0300

    parameter and argument

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function introductionWithLanguage(name, language){
   console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
       return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
+  //default parameter
 
 function introductionWithLanguageOptional(name, language = "JavaScript"){
     console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-// function introductionWithLanguageOverride(name = "", language = "") {
-//       console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
-//       return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-//   }
+
+
+
